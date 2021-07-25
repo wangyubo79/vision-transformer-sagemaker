03aa814e75d658182bfa8f60681cdb231db44d3a
Erika Pelaez <e.pelaez.coyotl@gmail.com>
2021-07-25 09:02:26 -0700

    Changed the value of the variable version from '00000' to '1' to avoid the libprotobuf error that prevents the server to start

--- code/train.py
+++ code/train.py
@@ -9,10 +9,12 @@ from model import VisionTransformer
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    parser.add_argument('--validation',type=str, default=os.environ['SM_CHANNEL_VALIDATION']) 
+
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+    parser.add_argument(
+        "--validation", type=str, default=os.environ["SM_CHANNEL_VALIDATION"]
+    )
     parser.add_argument("--image-size", type=int, default=32)
     parser.add_argument("--patch-size", type=int, default=4)
     parser.add_argument("--num-layers", type=int, default=4)
@@ -23,14 +25,14 @@ if __name__ == "__main__":
     parser.add_argument("--weight-decay", type=float, default=1e-4)
     parser.add_argument("--batch-size", type=int, default=256)
     parser.add_argument("--epochs", type=int, default=30)
- 
+
     args, _ = parser.parse_known_args()
-    
-    x_train = np.load(os.path.join(args.train, 'training.npz'))['image']
-    y_train = np.load(os.path.join(args.train, 'training.npz'))['label']
-    x_test  = np.load(os.path.join(args.validation, 'testing.npz'))['image']
-    y_test  = np.load(os.path.join(args.validation, 'testing.npz'))['label']
-     
+
+    x_train = np.load(os.path.join(args.train, "training.npz"))["image"]
+    y_train = np.load(os.path.join(args.train, "training.npz"))["label"]
+    x_test = np.load(os.path.join(args.validation, "testing.npz"))["image"]
+    y_test = np.load(os.path.join(args.validation, "testing.npz"))["label"]
+
     model = VisionTransformer(
         image_size=args.image_size,
         patch_size=args.patch_size,
@@ -43,9 +45,7 @@ if __name__ == "__main__":
         dropout=0.1,
     )
     model.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True
-        ),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         optimizer=tfa.optimizers.AdamW(
             learning_rate=args.learning_rate, weight_decay=args.weight_decay
         ),
@@ -57,8 +57,8 @@ if __name__ == "__main__":
         y_train,
         validation_data=(x_test, y_test),
         epochs=args.epochs,
-        verbose=2
+        verbose=2,
     )
-    
-    version = '00000000'
+
+    version = "1"
     model.save(os.path.join(args.model_dir, version))
